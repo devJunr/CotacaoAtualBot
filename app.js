2b841bb37620a8e87debeb8d3c8239e8e8c2f559
Adriano Jr <adrianojunr@gmail.com>
2025-01-26 20:14:24 -0300

    (feat) - Editar API, Editar gatilhos de ações

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
-const TOKEN_BOT = "<YOUR TOKEN TELEGRAM BOT>";
-var date = new Date();
-var ano = date.getFullYear();
-var mes = date.getMonth() + 1;
-var dia = date.getDate()-1;
-var urlAPI_Bcb = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao=%27";
-var urlAPI_Date = mes+"-"+dia+"-"+ano;
-var apiBcb = urlAPI_Bcb+urlAPI_Date+"%27&$top=100&$skip=0&$format=json&$select=cotacaoCompra,cotacaoVenda";
+const TOKEN_BOT = "<YOUR TOKEN TELEGRAM BOT ";
+var apiBcb = "https://economia.awesomeapi.com.br/json/all/USD-BRL";
 
 async function obterCotacao() {
     try {
-        const respBcb = await fetch(apiBcb);
-        if (respBcb.status === 200) {
-            const obj = await respBcb.json();
-            if (obj.value && obj.value.length > 0) {
-                const cotacao = obj.value[0];
-                return {
-                    cotacaoCompra: cotacao.cotacaoCompra.toFixed(2),
-                    cotacaoVenda: cotacao.cotacaoVenda.toFixed(2),
-                };
-            }
+        const respAPI = await fetch(apiBcb);
+        if (respAPI.status === 200) {
+            const obj = await respAPI.json();
+            const cotacao = obj.USD;
+            return {
+                bid: parseFloat(cotacao.bid).toFixed(2),
+                ask: parseFloat(cotacao.ask).toFixed(2),
+                high: parseFloat(cotacao.high).toFixed(2),
+                low: parseFloat(cotacao.low).toFixed(2),
+                create_date: cotacao.create_date,
+            };
         }
         return null;
     } catch (error) {
@@ -35,32 +30,32 @@ bot.on('message',async (msg) => {
 });
 
 bot.on('text',async (msg) => {
-  console.log('BOT ATIVO!');
   const compra = await obterCotacao();
   const venda = await obterCotacao();
   const chatId = msg.chat.id;
   const texto = msg.text;
 
   if (texto === '/start') {
-    bot.sendMessage(chatId, `👋 Olá! Eu sou um bot que fornece a cotação diária do dólar em real, com os valores de **fechamento do dia anterior** para compras e vendas.\n\n💡 Para saber a cotação atual, digite /cotacao.\n\n🛠️ Comandos disponíveis:\n- /cotacao: Ver a cotação do fechamento do dia anterior\n- /ajuda: Para obter mais informações sobre como usar o bot\n\n📱 Criado e administrado por @adrjun`);
+    bot.sendMessage(chatId, `👋 Olá! Eu sou um bot que fornece a cotação diária do dólar em real, com os valores de fechamento do dia anterior para compras e vendas.\n\n💡 Para saber a cotação atual, digite /dolar.\n\n🛠️ Comandos disponíveis:\n- /dolar: Ver a cotação atual em tempo real\n- /ajuda: Para obter mais informações sobre como usar o bot\n\n📱 Criado e administrado por @adrjun`);
   }
 
-  if(texto === '/cotacao'){
+  if(texto === '/dolar'){
     try {
         const cotacao = await obterCotacao();
         if (cotacao) {
-            bot.sendMessage(chatId, `Fechamento do dólar para o dia: ${dia}/${mes}/${ano}\n💵 Cotação do Dólar:\n📥 Compra: R$ ${cotacao.cotacaoCompra} - 📤 Venda: R$ ${cotacao.cotacaoVenda}\n\n💡 **Impacto do Dólar**: O valor do dólar afeta diretamente os preços de produtos importados e até o custo de viagens internacionais. O mercado de câmbio pode ser influenciado por diversos fatores, como mudanças na taxa de juros e decisões econômicas do governo.\n\n📊 🔗 Para mais informações sobre o mercado e acompanhar a cotação em tempo real, visite Banco Central: https://www.bcb.gov.br.\nPara obter mais detalhes ou ajuda, digite /ajuda. Para consultar a cotação novamente, basta digitar /cotacao.`);
+            bot.sendMessage(chatId, `📅 Última atualização em: ${cotacao.create_date}\n💵 Valor de compra atual do Dólar: R$ ${cotacao.bid}\n💵 Valor de venda atual do Dólar: R$ ${cotacao.ask}\n\n📊Oscilação do dia\n- Maior valor registrado hoje: R$${cotacao.high}\n- Menor valor registrado hoje: R$${cotacao.low}\n\nVocê já pode acompanhar a cotação em tempo real diretamente aqui! Para mais informações sobre o mercado, acesse o Banco Central em https://www.bcb.gov.br.\n🔍 Para mais detalhes ou ajuda, digite /ajuda .\n🔄 Para consultar a cotação novamente, digite /dolar .`);
+            
         } else {
             bot.sendMessage(chatId, `❌ Não foi possível obter a cotação do dólar no momento. Tente novamente mais tarde.`);
         }
     } catch (error) {
         console.error("Erro ao obter cotação:", error);
         bot.sendMessage(chatId, `❌ Ocorreu um erro ao obter a cotação. Tente novamente mais tarde.`);
-        
+        bot.sendMessage(7925045673, ` Aconteceu um erro no bot, código: ${error}`)
     }
   }
   
   if (texto === '/ajuda') {
-    bot.sendMessage(chatId, `🤖 **Como usar o bot**:\n\n- Para obter a cotação diária do dólar para compras e vendas, digite o comando /cotacao.\n- Se você deseja ver a mensagem inicial do bot novamente, digite /start.\n- Para visualizar essas opções de ajuda a qualquer momento, digite /ajuda.\n\n📣 **Contato**: Se precisar de ajuda, envie uma mensagem para @adrjun.\n\n🔍 **O que são o Dólar de Compra e o Dólar de Venda?**\n\n💰 O **dólar de compra** é o valor pelo qual um banco ou instituição financeira compra dólares de um cliente, enquanto o **dólar de venda** é o valor pelo qual o banco vende dólares para o cliente. Em geral, o dólar de venda é mais alto do que o de compra devido à margem de lucro do banco ou instituição financeira.\n\nExemplo: Se o dólar de compra for R$ 4,50 e o de venda for R$ 4,70, isso significa que o banco tem um spread de R$ 0,20 por dólar vendido.`);
+    bot.sendMessage(chatId, `🤖 Como usar o bot:\n\n- Para obter a cotação atual do dólar para compras e vendas, digite o comando /dolar.\n- Se você deseja ver a mensagem inicial do bot novamente, digite /start.\n- Para visualizar essas opções de ajuda a qualquer momento, digite /ajuda.\n\n📣 Contato: Se precisar de ajuda ou quer mandar uma sugestão, envie uma mensagem para @adrjun.\n\n🔍 O que são o Dólar de Compra e o Dólar de Venda?\n\n💰 O dólar de compra é o valor pelo qual um banco ou instituição financeira compra dólares de um cliente, enquanto o dólar de venda é o valor pelo qual o banco vende dólares para o cliente. Em geral, o dólar de venda é mais alto do que o de compra devido à margem de lucro do banco ou instituição financeira.\n\nExemplo: Se o dólar de compra for R$ 4,50 e o de venda for R$ 4,70, isso significa que o banco tem um spread de R$ 0,20 por dólar vendido.`);
     }
 });
